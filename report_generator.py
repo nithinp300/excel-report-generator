027603747ca850a7a4b8bad9d0503124c53d0ffe
Nithin Pingili <nithinp300@gmail.com>
2023-07-27 23:46:37 -0500

    sql connection

--- report_generator.py
+++ report_generator.py
@@ -6,8 +6,27 @@ import os
 #gui window
 sg.theme('SandyBeach')
 layout = [
-    [sg.Text("Please Enter SQL")]
+    [sg.Text("Please Enter SQL")],
+    [sg.Multiline(size=(50,5), key='textbook')],
+    [sg.Submit(), sg.Cancel()]
 ]
-window = sg.Window("Report Generator")
+window = sg.Window("Report Generator", layout)
 event, values = window.read()
 query = values['textbook']
+
+host1 = os.environ.get("host")
+user1 = os.environ.get("user")
+password1 = os.environ.get("password")
+
+
+#SQL connection/query
+cnx = mysql.connector.connect(
+    host = host1,
+    user = user1,
+    password = password1
+)
+
+#create and open report
+df = pd.read_sql(query, cnx)
+df.to_excel('report.xlsx', index=False)
+os.startfile('report.xlsx')
